2b95b5f395dba16459b5c42719d2003200fe3fb5
carlucios <carlucios@gmail.com>
2023-05-22 20:38:39 -0300

    Criação do projeto

---
+++ src/main/java/com/clsantos/desafio1/Desafio1Application.java
@@ -0,0 +1,18 @@
+package com.clsantos.desafio1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
